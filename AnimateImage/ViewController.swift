1ef9bdf0bbfcc62f7e9ae95779ee3842c0d82e53
otetLopez <otet.tud@gmail.com>
2020-01-08 17:12:21 -0500

    Apply functionality

--- AnimateImage/ViewController.swift
+++ AnimateImage/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var timer = Timer()
+    var curr_y : CGFloat = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        curr_y = imageView.frame.origin.y
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(animate), userInfo: nil, repeats: true)
+    }
+    
+    @objc func animate() {
+        let width = imageView.frame.size.width
+        let height = imageView.frame.size.height
+        let x = imageView.frame.origin.x
+        let y = imageView.frame.origin.y
+        
+        self.imageView.frame = CGRect(x: x, y: curr_y, width: width, height: height)
+        curr_y += 150
+        
+        let guide = view.safeAreaLayoutGuide
+        let heightMax = guide.layoutFrame.size.height
+        print("The max height is \(heightMax)")
+        if curr_y > guide.layoutFrame.size.height {
+            timer.invalidate()
+        }
+        
     }
 
 
