a40affd875b56da68ca2875b3de11e568ae504ed
otetLopez <otet.tud@gmail.com>
2020-01-08 23:32:46 -0500

    clockwise

--- AnimateImage/ViewController.swift
+++ AnimateImage/ViewController.swift
@@ -12,29 +12,112 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     var timer = Timer()
+    var x : CGFloat = 0
+    var y : CGFloat = 0
+    var initialX : CGFloat = 0
+    var initialY : CGFloat = 0
+    var curr_x : CGFloat = 0
     var curr_y : CGFloat = 0
+    var direction : Int = -1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        curr_y = imageView.frame.origin.y
+        x = imageView.frame.origin.x
+        y = imageView.frame.origin.y
+        curr_x = x
+        curr_y = y
         
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(animate), userInfo: nil, repeats: true)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+            swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+            view.addGestureRecognizer(swipeRight)
+            
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+            swipeRight.direction = UISwipeGestureRecognizer.Direction.left
+            view.addGestureRecognizer(swipeLeft)
     }
     
     @objc func animate() {
         let width = imageView.frame.size.width
         let height = imageView.frame.size.height
-        let x = imageView.frame.origin.x
         
-        self.imageView.frame = CGRect(x: x, y: curr_y, width: width, height: height)
+        self.imageView.frame = CGRect(x: curr_x, y: curr_y, width: width, height: height)
         curr_y += 150
 
         if curr_y > view.safeAreaLayoutGuide.layoutFrame.size.height {
             timer.invalidate()
+            curr_y = view.safeAreaLayoutGuide.layoutFrame.size.height
         }
         
     }
-
+    
+    @objc func swiped (gesture: UISwipeGestureRecognizer) {
+        initialX = imageView.frame.origin.x
+        initialY = imageView.frame.origin.y
+        
+        let swipeGesture = gesture as UISwipeGestureRecognizer
+        switch swipeGesture.direction {
+        case UISwipeGestureRecognizer.Direction.right:
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(animateClockwise), userInfo: nil, repeats: true)
+        case UISwipeGestureRecognizer.Direction.left:
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(animateCounterClockwise), userInfo: nil, repeats: true)
+        default:
+            break
+        }
+    }
+    
+    @objc func animateCounterClockwise() {
+        print("Swiped left and we are turing Counter Clockwise")
+        let width = imageView.frame.size.width
+        let height = imageView.frame.size.height
+        
+    }
+    
+    @objc func animateClockwise() {
+        
+        let width = imageView.frame.size.width
+        let height = imageView.frame.size.height
+        
+        let maxW = view.safeAreaLayoutGuide.layoutFrame.size.width
+        let maxH = view.safeAreaLayoutGuide.layoutFrame.size.height
+        print("Swiped right and we are turing Clockwise with \(maxW) \(maxH).")
+      
+        self.imageView.frame = CGRect(x: curr_x, y: curr_y, width: width, height: height)
+        
+        if curr_y == y {
+            if curr_x != maxW-50 {
+                curr_x += 50
+                if curr_x >= maxW {
+                    curr_x = maxW-50
+                    print("Reached Max")
+                }
+            } else {
+                curr_y += 50
+            }
+        } else if curr_y > y  {
+            if curr_x == maxW-50 && curr_y != maxH {
+                print("Should be here")
+                curr_y += 50
+              if curr_y >= maxH {
+                  curr_y = maxH
+              }
+            } else if curr_x != x {
+                curr_x -= 50
+                if curr_x <= x {
+                curr_x = x
+                }
+            } else {
+                curr_y -= 50
+                if curr_y <= y {
+                    curr_y = y
+                }
+            }
+        }
+        if curr_y > initialY && curr_x == initialX {
+            timer.invalidate()
+        }
+    }
 
 }
 
